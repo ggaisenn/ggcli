f4b2d1d28eb6a15a6b2204d9b5c188c17e744a37
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-06-27 21:55:28 +0530

    Parsed CLI Arguments
    arg package receives the object which states the arguments it can receive and their types
    added boolean arguements

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
 // Interpreter used to execute our script
 // Runs the tool with various arguments
-console.log(process.argv);
+//console.log(process.argv);
+
+//To parse command line arguements
+const arg = require('arg');
+
+const args = arg({
+    '--open': Boolean,
+    '--buildcheck': Boolean,
+});
+
+if(args['--open']){
+    console.log("Opening the app..");
+}
