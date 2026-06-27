e52ee5aca77ad7b2b15dbfefdb52203b7c2a654d
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-06-27 22:11:08 +0530

    Add EH and guide features Added Exception Handling to CLI Arguements Added a guide feature to provide user guide in case of exceptions being caught

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -6,11 +6,25 @@
 //To parse command line arguements
 const arg = require('arg');
 
-const args = arg({
-    '--open': Boolean,
-    '--buildcheck': Boolean,
-});
+try{
+    const args = arg({
+     '--open': Boolean,
+     '--buildcheck': Boolean,
+    });
+    
+    if(args['--open']){
+     console.log("Opening the app..");
+    }
+    if(args['--buildcheck']){
+        console.log("Providing app info..")
+    }
+}catch(e){
+    console.log(e.message);
+    console.log();
+    gguide()
+}
 
-if(args['--open']){
-    console.log("Opening the app..");
+function gguide(){
+    console.log("--open: Opens the app")
+    console.log("--buildcheck: Check the app's build")
 }
