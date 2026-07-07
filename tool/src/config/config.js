58acdb5d6d7026b37baa0d047c91bb00e8036270
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-07-07 10:57:59 +0530

    Replace config manager from package-up to cosmicconfig
    Replaced package-up logic with cosmicconfig to handle tool configuration
    Enables  support for various file formats, async search, and caching

--- tool/src/config/config.js
+++ tool/src/config/config.js
@@ -1,14 +1,15 @@
 import chalk from 'chalk';
-import { packageUp } from 'package-up'; 
+import {cosmiconfigSync} from 'cosmiconfig';
+const configLoad = cosmiconfigSync('tool');
 import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
 
 export default async function getConfig() {
-    const pkgPath = await packageUp();
-    const pkg = require(pkgPath);
-    if (pkg.tool) {
-       return pkg.tool;
+    const result = configLoad.search(process.cwd());
+    if (result) {
+        console.log(chalk.green('Configuration found'));
+        return result.config;
     } else {
         console.log(chalk.yellow('Could not find configuration, using Default configuration'));
         return {port: 1234};
