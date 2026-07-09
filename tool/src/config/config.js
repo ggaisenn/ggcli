07d010be9264d2fd704266caed13a08c59a123a0
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-07-09 17:08:07 +0530

    feat(config.js): add schema validation for configuration using ajv

--- tool/src/config/config.js
+++ tool/src/config/config.js
@@ -2,12 +2,23 @@ import chalk from 'chalk';
 import {cosmiconfigSync} from 'cosmiconfig';
 const configLoad = cosmiconfigSync('gg-cli');
 import { createRequire } from 'module';
+import schema from './schema.json';
+import Ajv from 'ajv';
+const ajv = new Ajv();
+
+
 
 const require = createRequire(import.meta.url);
 
 export default async function getConfig() {
     const result = configLoad.search(process.cwd());
     if (result) {
+        const isvalid = ajv.validate(schema, result.config);
+        if(!isvalid){
+            console.log(chalk.red('Invalid configuration'));
+            console.log(chalk.red(ajv.errors));
+            process.exit(1);
+        }
         console.log(chalk.green('Configuration found'));
         return result.config;
     } else {
