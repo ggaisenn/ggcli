73245224e82bc536c7668331854d661544155d74
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-07-09 18:13:57 +0530

    feat(logger.js): Add warning, highlight, and debug methods to logger.js

--- tool/src/logger.js
+++ tool/src/logger.js
@@ -3,6 +3,9 @@ import chalk from 'chalk';
 
 export default async function Logger(name){
     return {
-
+        log: (...args) => console.log(chalk.gray(...args)),
+        warning: (...args) => console.log(chalk.yellow(...args)), 
+        highlight: (...args) => console.log(chalk.blue(...args)),
+        debug: console.log
     };
 }
