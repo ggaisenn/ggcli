4b6da54ef07b6c7612d46e46ad84ff441e24b24e
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-07-09 19:00:21 +0530

    feat(logger.js): Add error method to logger.js

--- tool/src/logger.js
+++ tool/src/logger.js
@@ -6,6 +6,7 @@ export default function Logger(name){
         log: (...args) => console.log(chalk.gray(...args)),
         warning: (...args) => console.log(chalk.yellow(...args)), 
         highlight: (...args) => console.log(chalk.blue(...args)),
+        error: (...args) => console.log(chalk.red(...args)),
         debug: (...args) => console.log(chalk.green(...args))
     };
 }
