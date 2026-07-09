7f98b9b15675f19651d4be2d12d0c35ec8b488c5
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-07-09 18:29:01 +0530

    fix(logger.js): remove async keyword from logger.js to fix runtime error

--- tool/src/logger.js
+++ tool/src/logger.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 
 
-export default async function Logger(name){
+export default function Logger(name){
     return {
         log: (...args) => console.log(chalk.gray(...args)),
         warning: (...args) => console.log(chalk.yellow(...args)), 
