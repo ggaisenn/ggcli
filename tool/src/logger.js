eaed04e67fee55f303b93e9266810891120ffb06
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-07-09 19:40:51 +0530

    style(logger.js): update colour in log method

--- tool/src/logger.js
+++ tool/src/logger.js
@@ -1,12 +1,12 @@
 import chalk from 'chalk';
-
+import debug from 'debug';
 
 export default function Logger(name){
     return {
-        log: (...args) => console.log(chalk.gray(...args)),
+        log: (...args) => console.log(chalk.green(...args)),
         warning: (...args) => console.log(chalk.yellow(...args)), 
         highlight: (...args) => console.log(chalk.blue(...args)),
         error: (...args) => console.log(chalk.red(...args)),
-        debug: (...args) => console.log(chalk.green(...args))
+        debug: debug(name)
     };
 }
