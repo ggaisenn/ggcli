da6d4ec21c0d3be4e609c7a31198d0f7e1a59af1
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-07-09 19:38:10 +0530

    fix(open.js): update logger methods in open.js

--- tool/src/command/open.js
+++ tool/src/command/open.js
@@ -3,6 +3,6 @@ import Logger from '../logger.js';
 const logger = Logger('open.js');
 
 export default async function open(config) {
-  logger.debug("Opening the app..");
-  logger.log('Received configuration in open -', JSON.stringify(config));
+  logger.log("Opening the app..");
+  logger.debug('Received configuration in open -', JSON.stringify(config));
 }
